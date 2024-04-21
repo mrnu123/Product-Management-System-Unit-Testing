9e1a6247e682804ad8d263fdaa7057468ef9d750
anuchit <anuchit.nu567@gmail.com>
2024-04-21 13:16:10 +0700

    add post products

--- app.test.js
+++ app.test.js
@@ -9,3 +9,25 @@ describe("GET /products", function () {
       .expect(200, done);
   });
 });
+
+describe("POST /products", function () {
+  it("responds with json", function (done) {
+    const expectedData = {
+      name: "Strawberry",
+      category: "Fruit",
+      price: 150,
+      stock: 20,
+    };
+    request(app)
+      .post("/products")
+      .send(expectedData)
+      .set("Acccept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).toEqual(expect.objectContaining(expectedData));
+        done();
+      });
+  });
+});
